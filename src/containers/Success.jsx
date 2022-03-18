a4ea7d3551187094291565f79ba0aa9597e2ea71
carlosed764 <carlosed7641@gmail.com>
2022-03-18 13:57:23 -0500

    api google maps

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+  const address = useGoogleAddress(buyer[0].address);
+
   return (
-    <div className="Succes">
+    <div className="Success">
       <div className="Success-content">
-        <h2>Nombre, Gracias por tu compra</h2>
-        <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-        <div className="Success-map">Google Maps</div>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+        <div className="Success-map">
+          <Map data={address} />
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Success;
